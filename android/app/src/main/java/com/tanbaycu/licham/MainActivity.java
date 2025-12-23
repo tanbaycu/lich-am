40ea8a992fdac6ece9f442813ca2e7ee27c6213b
tanbaycu <mnew20128@gmail.com>
2025-12-23 21:30:13 +0700

    feat: add capacitor support and automatic android build workflow

---
+++ android/app/src/main/java/com/tanbaycu/licham/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tanbaycu.licham;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tanbaycu.licham',
+  appName: 'Lịch Âm',
+  webDir: 'dist'
+};
+
+export default config;
